4013f0fdec41a7c767de7c2fd730ec4597184351
Akif Karataş <akifkrts.3883@gmail.com>
2026-03-28 01:39:30 +0300

    Dart Öğrenimimin 2. Günü
    Dart öğrenimimin 2. gününde "print" dizisinin kullanım şekillerini inceledim.

---
+++ dart_ogreniyorum/dart_ogreniyorum_2.dart
@@ -0,0 +1,40 @@
+void main(List<String> args) {
+  print("Dart öğreniyorum.");
+
+  print('Dart öğreniyorum.');
+
+  print("Dart " + "öğreniyorum.");
+
+  print('Dart ' + 'öğreniyorum.');
+
+  print("Dart " + 'öğreniyorum.');
+
+  print("Dart Dili'ni öğreniyorum.");
+  
+    /*
+    -----------------------------------------------------------------------------------------
+    ÖNEMLİ NOT:
+    -----------------------------------------------------------------------------------------
+    print işlemi yaparken tek tırnak ve çift tırnak arasında fark yoktur.
+    İkisi de kullanılabilir. Ancak tek tırnak kullanırken içinde tek tırnak varsa hata verir.
+    Aynı şekilde, çift tırnak kullanırken içinde çift tırnak varsa hata verir.
+    Bu nedenle bazı teknik hatalar oluşur. Bu hataları önlemek için "\" karakteri kullanılır.
+    -----------------------------------------------------------------------------------------
+    */
+
+  print("Dart Dili\'ni öğreniyorum.");
+
+  print("3 * 5");
+
+  /*
+  ------------------------------------------------------------------------------------------------------------
+  ÖNEMLİ NOT:
+  ------------------------------------------------------------------------------------------------------------
+  Eğer print işlemi içerisinde matematiksel bir işlem yapmak istiyorsak, işlemi tırnak içine almamalıyız.
+  Aksi takdirde, işlemi metin olarak algılar ve matematiksel işlem yapmaz. Bu nedenle, matematiksel işlemleri
+  tırnak içine almadan yazmalıyız.
+  ------------------------------------------------------------------------------------------------------------
+   */
+
+  print(3 * 5);
+  }
